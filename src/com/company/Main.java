04adf0471511e769d8aa925e1484b93f9d5c5e20
anna <annabugrinskaya@gmail.com>
2022-01-26 20:17:16 -0500

    here is the green car

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
         //the basic structure
         carBasics carBasics = new carBasics();
-        carBasics.color = "red";
-        carBasics.engine = "7999cc";
-        carBasics.door = "2";
-        carBasics.number = "22222";
+        carBasics.color = "green";
+        carBasics.number = "25555";
+        carBasics.door = "3";
+        carBasics.engine = "4589cc";
         System.out.println(carBasics.color);
     }
 }
