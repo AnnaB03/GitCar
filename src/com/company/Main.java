2d477b3cc03563bda4318048caad7f04ed9fff14
anna <annabugrinskaya@gmail.com>
2022-01-26 19:56:17 -0500

    the red car is ready for the trial

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,10 @@ public class Main {
     public static void main(String[] args) {
         //the basic structure
         carBasics carBasics = new carBasics();
-        
+        carBasics.color = "red";
+        carBasics.engine = "7999cc";
+        carBasics.door = "2";
+        carBasics.number = "22222";
+        System.out.println(carBasics.color);
     }
 }
