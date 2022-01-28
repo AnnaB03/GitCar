e1b16dc4f444d7fae567aaaec167afe38023b995
anna <annabugrinskaya@gmail.com>
2022-01-28 18:16:03 -0500

    latest commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,17 @@ public class Main {
     public static void main(String[] args) {
         //the basic structure
         carBasics carBasics = new carBasics();
+
         carBasics.color = "green";
         carBasics.number = "25555";
         carBasics.door = "3";
         carBasics.engine = "4589cc";
+
+        carBasics.color = "blue";
+        carBasics.engine = "5236cc";
+        carBasics.door = "4";
+        carBasics.number = "4568977";
+
         System.out.println(carBasics.color);
     }
 }
